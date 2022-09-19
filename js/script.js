5ec7d34206eedd32c939fa010630e0b09853b84c
MargaritaVin <ritasergienko@rambler.ru>
2022-09-19 21:51:13 +0300

    correction

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const form = document.querySelector('form')
-const button = document.querySelector('button')
-const text = document.querySelector('input')
-const list = document.getElementById('list')
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+const text = document.querySelector('input');
+const list = document.getElementById('list');
+const btn = document.querySelector('.btnDelete');
+const checkbox = document.querySelector('.checkbox');
+console.log(btn)
 
 
 form.onsubmit = (event) => {
     event.preventDefault();
     if (text.value.trim().length !== 0) {
-        let li = document.createElement('li');
-        let liButton = document.createElement('button');
-        liButton.innerHTML = "Delete";
-        liButton.className = "btnDelete"
-        li.append(text.value, liButton);
-        list.append(li);
+        list.append(
+            createTodo(text.value),
+            );
         text.value = '';
         return
     } else {
@@ -22,10 +22,37 @@ form.onsubmit = (event) => {
     }
 };
 
+const createTodo = (value) => {
+    let li = document.createElement('li');
+    let liButton = document.createElement('button');
+    let checkbox = document.createElement('INPUT');
+    checkbox.setAttribute("type", "checkbox");
+    liButton.innerHTML = "Delete";
+    liButton.className = "btnDelete";
+    li.prepend(checkbox);
+    li.append(value, liButton);
+    return li;
+};
+
+// const createTodo = (domNode) => {
+//     let li = document.createElement('li');
+//     li.append(domNode);
+//     return li;
+// };
+
+// const addTodo = () => {
+//     let liButton = document.createElement('button');
+//     liButton.innerHTML = "Delete";
+//     liButton.className = "btnDelete";
+//     const a = createTodo(text.value).append(liButton);
+//     const a = createTodo(liButton); // ??
+//     return a
+// }
+
 text.oninput = () => {
     const isErrorField = text.classList.contains("error");
     if (isErrorField) {
-        text.classList.remove("error")
+        text.classList.remove("error");
         list.innerHTML = "";
     }
 };
@@ -38,3 +65,8 @@ list.addEventListener("click", (event) => {
     }
 });
 
+
+// checkbox.onchange = (event) => {
+//     const isAccepted = event.target.checked;
+//     btn.disabled = !isAccepted;
+//   };
