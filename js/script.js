aa9efbe31c6192460f8218bbbe0fb4b248966863
MargaritaVin <ritasergienko@rambler.ru>
2022-09-20 16:02:46 +0300

    DOM: Practice

--- js/script.js
+++ js/script.js
@@ -23,7 +23,11 @@ const createTodo = (value) => {
     let li = document.createElement('li');
     let liButton = document.createElement('button');
     liButton.innerHTML = "Delete";
-    liButton.className = "btnDelete";
+    liButton.className = "btn-delete";
+    let checkbox = document.createElement('INPUT');
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.className = "form-check-input";
+    li.prepend(checkbox);
     li.append(value, liButton);
     return li;
 };
@@ -38,9 +42,17 @@ text.oninput = () => {
 };
 
 list.addEventListener("click", (event) => {
-    const isDeleteButton = event.target.className === "btnDelete";
+    const isDeleteButton = event.target.className === "btn-delete";
     if (isDeleteButton) {
         const liContentBlock = event.target.closest("li");
         liContentBlock.remove();
     }
 });
+
+list.onchange = (event) => {
+    const checkbox = event.target;
+    checkbox.setAttribute('disabled', 'disabled');  // or checkbox.disabled = true;
+    const liContentBlock = event.target.closest("li");
+    const button = liContentBlock.querySelector("button");
+    button.setAttribute('disabled', 'disabled');  // or button.disabled = true;
+  };
